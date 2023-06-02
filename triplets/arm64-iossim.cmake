01dc8e9831b6e19a09c4c9ca75a566e2116324b8
Edoardo Lolletti <edoardo762@gmail.com>
2023-06-02 22:51:30 +0200

    Build for ios simulator x64/aarch64 only for now
    [skip-win22][skip-osx-cctools][skip-osx64-cctools][skip-win][skip-win15][skip-win17][skip-win19][skip-osx][skip-osx64][skip-linux][skip-linux64][skip-osx-sdk][skip-linux-docker-gcc7][do-irr][skip-ios-cctools][skip-iosv7-cctools][new]

--- triplets/arm64-iossim.cmake
+++ triplets/arm64-iossim.cmake
@@ -4,7 +4,7 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(CMAKE_RANLIB /opt/cctools/bin/arm-apple-darwin11-ranlib)
 set(CMAKE_AR /opt/cctools/bin/arm-apple-darwin11-ar)
 set(VCPKG_OSX_SYSROOT /opt/cctools/sdk/iPhoneSimulator.sdk)
-set(VCPKG_LINKER_FLAGS /opt/cctools/darwin/libclang_rt.ios.a)
+set(VCPKG_LINKER_FLAGS /opt/cctools/darwin/libclang_rt.iossim.a)
 
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME iOS)
