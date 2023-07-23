f98f9a5b580805f23d16f4e29b0d68547975b66b
Edoardo Lolletti <edoardo762@gmail.com>
2023-07-23 15:26:30 +0200

    Update openal-soft
    From 1.21.1 to 1.23.0

--- ports/openal-soft/portfile.cmake
+++ ports/openal-soft/portfile.cmake
@@ -1,11 +1,16 @@
-vcpkg_fail_port_install(ON_TARGET "UWP")
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO kcat/openal-soft
-    REF ae4eacf147e2c2340cc4e02a790df04c793ed0a9 # openal-soft-1.21.1
-    SHA512 6ba006d3dad6efe002f285ff509a59f02b499ec3f6065df12a89c52355464117b4dbabcd04ee9cbf22cc3b4125c8e456769b172f8c3e9ee215e760b2c51a0a8f
+    REF ${VERSION}
+    SHA512 7384e734ba6b0668adbb2b2629c950bdb61814584a745ced2327cc20b1b9ff9bc53a8e10ec3260ef2a2915048f4e3af8499d91f8515bb18a4e61c5eeef609d1a
     HEAD_REF master
+    PATCHES
+      c12ada68951ea67a59bef7d4fcdf22334990c12a.patch # Merged upstream, remove in next version
+)
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        pipewire  ALSOFT_BACKEND_PIPEWIRE
 )
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
@@ -17,6 +22,7 @@ endif()
 set(ALSOFT_REQUIRE_LINUX OFF)
 set(ALSOFT_REQUIRE_WINDOWS OFF)
 set(ALSOFT_REQUIRE_APPLE OFF)
+set(ALSOFT_CPUEXT_NEON OFF)
 
 if(VCPKG_TARGET_IS_LINUX)
     set(ALSOFT_REQUIRE_LINUX ON)
@@ -27,15 +33,17 @@ endif()
 if(VCPKG_TARGET_IS_OSX OR VCPKG_TARGET_IS_IOS)
     set(ALSOFT_REQUIRE_APPLE ON)
 endif()
+if(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64")
+    set(ALSOFT_CPUEXT_NEON ON)
+endif()
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    OPTIONS
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS ${FEATURE_OPTIONS}
         -DLIBTYPE=${OPENAL_LIBTYPE}
         -DALSOFT_UTILS=OFF
         -DALSOFT_NO_CONFIG_UTIL=ON
         -DALSOFT_EXAMPLES=OFF
-        -DALSOFT_TESTS=OFF
         -DALSOFT_CONFIG=OFF
         -DALSOFT_HRTF_DEFS=OFF
         -DALSOFT_AMBDEC_PRESETS=OFF
@@ -43,37 +51,58 @@ vcpkg_configure_cmake(
         -DALSOFT_BACKEND_OSS=OFF
         -DALSOFT_BACKEND_SOLARIS=OFF
         -DALSOFT_BACKEND_SNDIO=OFF
-        -DALSOFT_BACKEND_QSA=OFF
         -DALSOFT_BACKEND_PORTAUDIO=OFF
         -DALSOFT_BACKEND_PULSEAUDIO=${ALSOFT_REQUIRE_LINUX}
         -DALSOFT_BACKEND_COREAUDIO=${ALSOFT_REQUIRE_APPLE}
-        -DALSOFT_REQUIRE_COREAUDIO=${ALSOFT_REQUIRE_APPLE}
         -DALSOFT_BACKEND_JACK=OFF
-        -DALSOFT_BACKEND_OPENSL=OFF
+        -DALSOFT_BACKEND_OPENSL=${VCPKG_TARGET_IS_ANDROID}
         -DALSOFT_BACKEND_WAVE=ON
         -DALSOFT_BACKEND_WINMM=${ALSOFT_REQUIRE_WINDOWS}
         -DALSOFT_BACKEND_DSOUND=${ALSOFT_REQUIRE_WINDOWS}
         -DALSOFT_REQUIRE_WASAPI=${ALSOFT_REQUIRE_WINDOWS}
-        -DALSOFT_CPUEXT_NEON=OFF
+        -DALSOFT_CPUEXT_NEON=${ALSOFT_CPUEXT_NEON}
         -DCMAKE_DISABLE_FIND_PACKAGE_WindowsSDK=ON
+    MAYBE_UNUSED_VARIABLES
+        ALSOFT_AMBDEC_PRESETS
+        ALSOFT_BACKEND_ALSA
+        ALSOFT_BACKEND_COREAUDIO
+        ALSOFT_BACKEND_JACK
+        ALSOFT_BACKEND_OPENSL
+        ALSOFT_BACKEND_OSS
+        ALSOFT_BACKEND_PORTAUDIO
+        ALSOFT_BACKEND_PIPEWIRE
+        ALSOFT_BACKEND_PULSEAUDIO
+        ALSOFT_BACKEND_SNDIO
+        ALSOFT_BACKEND_SOLARIS
+        ALSOFT_CONFIG
+        ALSOFT_CPUEXT_NEON
+        ALSOFT_HRTF_DEFS
+        ALSOFT_BACKEND_WINMM
+        ALSOFT_BACKEND_DSOUND
+        CMAKE_DISABLE_FIND_PACKAGE_WindowsSDK
 )
 
-vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/OpenAL)
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/OpenAL")
 
 foreach(HEADER al.h alc.h)
-    file(READ ${CURRENT_PACKAGES_DIR}/include/AL/${HEADER} AL_H)
+    file(READ "${CURRENT_PACKAGES_DIR}/include/AL/${HEADER}" AL_H)
     if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
         string(REPLACE "defined(AL_LIBTYPE_STATIC)" "1" AL_H "${AL_H}")
     else()
-        string(REPLACE "defined(AL_LIBTYPE_STATIC)" "0" AL_H "${AL_H}")
+        # Normally we would say:
+        # string(REPLACE "defined(AL_LIBTYPE_STATIC)" "0" AL_H "${AL_H}")
+        # but we are leaving these undefined macros alone in support of
+        # https://github.com/microsoft/vcpkg/issues/18098
     endif()
-    file(WRITE ${CURRENT_PACKAGES_DIR}/include/AL/${HEADER} "${AL_H}")
+    file(WRITE "${CURRENT_PACKAGES_DIR}/include/AL/${HEADER}" "${AL_H}")
 endforeach()
+
+vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
-file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/usage DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 
 vcpkg_copy_pdbs()
