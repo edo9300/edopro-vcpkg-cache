88480cd30df861ec21a06edee1305bdac02129fb
Edoardo Lolletti <edoardo762@gmail.com>
2024-05-12 20:13:18 +0200

    Properly use cmake toolchains for cctools targets
    [new][skip-win][skip-win15][skip-win17][skip-win19][skip-win22][skip-linux][skip-linux64][skip-osx][skip-osx64]

--- triplets/arm-ios.cmake
+++ triplets/arm-ios.cmake
@@ -1,10 +1,8 @@
 set(VCPKG_TARGET_ARCHITECTURE arm)
 set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
 
-set(CMAKE_RANLIB /opt/cctools/bin/arm-apple-darwin11-ranlib)
-set(CMAKE_AR /opt/cctools/bin/arm-apple-darwin11-ar)
-set(VCPKG_OSX_SYSROOT /opt/cctools/sdk/iPhoneOS.sdk)
-set(VCPKG_LINKER_FLAGS /opt/cctools/darwin/libclang_rt.ios.a)
+set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/../toolchains/arm-ios.cmake")
 
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME iOS)
--- triplets/x64-iossim.cmake
+++ triplets/x64-iossim.cmake
@@ -1,10 +1,8 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
 
-set(CMAKE_RANLIB /opt/cctools/bin/x86_64-apple-darwin11-ranlib)
-set(CMAKE_AR /opt/cctools/bin/x86_64-apple-darwin11-ar)
-set(VCPKG_OSX_SYSROOT /opt/cctools/sdk/iPhoneSimulator.sdk)
-set(VCPKG_LINKER_FLAGS /opt/cctools/darwin/libclang_rt.iossim.a)
+set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/../toolchains/x64-iossim.cmake")
 
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME iOS)
