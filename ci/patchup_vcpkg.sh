292c044419d3d7dcdffa0b7012fab08d9739c941
Edoardo Lolletti <edoardo762@gmail.com>
2026-03-27 10:30:01 +0100

    remove vs2019 generator and move vs2017 xp to vs2022
    [new]

--- ci/patchup_vcpkg.sh
+++ ci/patchup_vcpkg.sh
@@ -19,24 +19,19 @@ if [[ $ARCHIVE_NAME == "installed_x86-windows-static.zip" ]]; then
 	# vcpkg doesn't like when we pass an _xp toolset to it
 	# so make the v141_Xp toolset the v141 and pass it to the triplet
 	pushd .
-	cd "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Microsoft\VC\v150\Platforms\Win32\PlatformToolsets"
+	cd "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\MSBuild\Microsoft\VC\v150\Platforms\Win32\PlatformToolsets"
 	rm -r v141 || true
 	mv v141_xp v141
 	popd
 	rm ./ports/libssh2/portfile.cmake
-	mv ./ports/libssh2/portfile-force-vs2019-generator-2017-toolset.cmake ./ports/libssh2/portfile.cmake
+	mv ./ports/libssh2/portfile-force-vs2022-generator.cmake ./ports/libssh2/portfile.cmake
 	rm ./ports/openal-soft/portfile.cmake
-	mv ./ports/openal-soft/portfile-force-vs2019-generator-2017-toolset.cmake ./ports/openal-soft/portfile.cmake
+	mv ./ports/openal-soft/portfile-force-vs2022-generator.cmake ./ports/openal-soft/portfile.cmake
 elif [[ $ARCHIVE_NAME == "installed_x86-windows-static-vs2017.zip" ]]; then
 	rm ./ports/libssh2/portfile.cmake
 	mv ./ports/libssh2/portfile-force-vs2022-generator.cmake ./ports/libssh2/portfile.cmake
 	rm ./ports/openal-soft/portfile.cmake
 	mv ./ports/openal-soft/portfile-force-vs2022-generator.cmake ./ports/openal-soft/portfile.cmake
-elif [[ $ARCHIVE_NAME == "installed_x86-windows-static-vs2015.zip" ]]; then
-	rm ./ports/libssh2/portfile.cmake
-	mv ./ports/libssh2/portfile-force-vs2019-generator.cmake ./ports/libssh2/portfile.cmake
-	rm -r ./ports/openal-soft
-	mv ./ports/openal-soft-vs2015 ./ports/openal-soft
 fi
 
 
